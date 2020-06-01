369e52ad91342e6c32fd015fe28e0ecbffe231fd
NehemiahLimo <nehemiahlimo02@gmail.com>
2020-06-01 23:35:38 +0300

    Added Spinkit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:weather/screens/location_screen.dart';
+import 'package:weather/screens/location_screen.dart';
 import 'package:weather/services/location.dart';
 import 'package:weather/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 const apikey ='9c69d0ea639c843995e6d84e92763c87';
@@ -19,41 +21,39 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   double lat;
   double long;
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
-
-    getLocation()
-    ;
+    getLocationData();
   }
-
-
 
 
   void getLocationData() async {
-    Location location=new Location();
+    Location location = new Location();
     await location.getCurrentLocation();
-    lat=location.lat;
+    lat = location.lat;
     long = location.long;
 
-    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apikey');
-    var weatherData=  await  networkHelper.getData();
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apikey');
+    var weatherData = await networkHelper.getData();
 
-
-
-
-
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen();
+    }));
   }
 
-
-
-  }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
-
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
     );
   }
 }
